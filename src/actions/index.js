c4d955a582e628ec7cdab588c51b5ba4983a4e9a
Clayton <clayton.weller@gmail.com>
2020-05-09 12:57:27 -0600

    most of bootcamp

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,26 +1,27 @@
-import { performanceActionHash } from './performance'
-import { utilitiesActionHash } from './utilities'
-import { moduleActionHash } from './modules'
+import { performanceActionHash } from './performance';
+import { utilitiesActionHash } from './utilities';
+import { moduleActionHash } from './modules';
 
 export const manageMessage = async (message, component) => {
-  let action = Object.keys(actionHash).includes(message.action)
+  console.log('Action: ', message.action);
+  const action = Object.keys(actionHash).includes(message.action)
     ? actionHash[message.action]
-    : actionHash.defaultAction
+    : actionHash.defaultAction;
 
   try {
-    return await action(message.params, component)
+    return await action(message.params, component);
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
-}
+};
 
 const actionHash = {
   ...utilitiesActionHash,
   ...performanceActionHash,
   ...moduleActionHash,
-  defaultAction
+  defaultAction,
+};
+
+async function defaultAction(params, component) {
+  console.log('DEFAULT \n', params, component.state);
 }
-
-async function defaultAction(params, component) { console.log('DEFAULT \n', params, component.state) }
-
-
