e4c95733a32dc4f4694d52b8d260b4fd685cb45a
Clayton <clayton.weller@gmail.com>
2020-06-12 09:11:09 -0600

    beginning question logic

--- src/components/modules/Trolly/index.js
+++ src/components/modules/Trolly/index.js
@@ -1,11 +1,12 @@
-
 import React from 'react';
 
 export function Trolly({ moduleState }) {
+  let display = <div>Look at the stage</div>;
+
   return (
     <div>
       <h3>Trolly</h3>
-      <p>This is a module template</p>
+      {display}
     </div>
   );
 }
